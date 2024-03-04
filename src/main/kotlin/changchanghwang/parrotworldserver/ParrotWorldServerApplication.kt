a296e4e20a20fb29f359e8456f5cca7db1270617
changhwan hwang <ch.hwang@ecubelabs.com>
2024-03-04 16:32:08 +0900

    chore: Initial commit

---
+++ src/main/kotlin/changchanghwang/parrotworldserver/ParrotWorldServerApplication.kt
@@ -0,0 +1,11 @@
+package changchanghwang.parrotworldserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParrotWorldServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ParrotWorldServerApplication>(*args)
+}
---
+++ src/test/kotlin/changchanghwang/parrotworldserver/ParrotWorldServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package changchanghwang.parrotworldserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParrotWorldServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
