40f7b301b2206958fc807f93e371b4b1074e06c3
changhwan hwang <ch.hwang@ecubelabs.com>
2024-03-19 23:14:57 +0900

    fix: dto validation

--- src/main/kotlin/changchanghwang/parrotworldserver/services/verifcation/dto/VerificationDto.kt
+++ src/main/kotlin/changchanghwang/parrotworldserver/services/verifcation/dto/VerificationDto.kt
@@ -1,10 +1,15 @@
 package changchanghwang.parrotworldserver.services.verifcation.dto
 
 import changchanghwang.parrotworldserver.services.verifcation.domain.VerificationType
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
 
 class VerificationDto {
     data class StartRequest(
+        @field:NotBlank(message = "이메일을 입력해주세요.")
+        @field:Email(message = "이메일 형식이 올바르지 않습니다.")
         val email: String,
+        @field:NotBlank(message = "인증 타입을 입력해주세요")
         val type: VerificationType,
     )
 
@@ -13,6 +18,7 @@ class VerificationDto {
     )
 
     data class ConfirmRequestBody(
+        @field:NotBlank(message = "인증 코드를 입력해주세요.")
         val code: String,
     )
 }
